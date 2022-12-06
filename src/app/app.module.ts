9d8bb4277492d6d8ecb94be94f669bb42a8c055f
hungl <hungl@kbsec.com.vn>
2022-12-06 10:11:39 +0700

    nah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+// Basic Stuff was imported here 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+// End of basic stuff
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialExampleModule } from 'material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
